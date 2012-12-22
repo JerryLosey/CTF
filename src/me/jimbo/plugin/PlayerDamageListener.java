1af4be405af727e599f593e5c657340cd07a94bf
loseyjerry <jlosey90@gmail.com>
2012-12-22 18:36:35 -0500

    Test for canDamage boolean.  Players can only damage/break/place after the countdown!

--- src/me/jimbo/plugin/PlayerDamageListener.java
+++ src/me/jimbo/plugin/PlayerDamageListener.java
@@ -1,5 +1,7 @@
 package me.jimbo.plugin;
 
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,7 +13,6 @@ import org.bukkit.inventory.PlayerInventory;
 
 public class PlayerDamageListener implements Listener{
 	
-	@SuppressWarnings("unused")
 	private CTF plugin;
 	
 	public PlayerDamageListener (CTF plugin) {
@@ -20,42 +21,67 @@ public class PlayerDamageListener implements Listener{
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerDamage(EntityDamageEvent e) {
-		if((e.getEntity() instanceof Player)){
-			Player p = (Player)e.getEntity();
-			if(CTF.PlayerClasses.containsKey(p)){
-				if(CTF.PlayerClasses.get(p).equals("soldier")){
-					if(e.getCause().equals(DamageCause.FALL)){
-						e.setCancelled(true);
+		if(plugin.canAttack){
+			if((e.getEntity() instanceof Player)){
+				Player p = (Player)e.getEntity();
+				if(CTF.PlayerClasses.containsKey(p)){
+					if(CTF.PlayerClasses.get(p).equals("soldier")){
+						if(e.getCause().equals(DamageCause.FALL)){
+							e.setCancelled(true);
+						}
 					}
 				}
+				if(p.getHealth() - e.getDamage() < 1){
+					PlayerInventory inv = p.getInventory();
+					inv.clear();
+					inv.setArmorContents(null);
+					p.setHealth(0);
+				}
 			}
-			if(p.getHealth() - e.getDamage() < 1){
-				PlayerInventory inv = p.getInventory();
-				inv.clear();
-				inv.setArmorContents(null);
-				p.setHealth(0);
-			}
+		}else{
+			e.setCancelled(true);
 		}
 	}
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerDamageEnt(EntityDamageByEntityEvent e) {
-		if((e.getEntity() instanceof Player)){
-			Player p = (Player)e.getEntity();
-			if(CTF.PlayerClasses.containsKey(p)){
-				if(CTF.PlayerClasses.get(p).equals("soldier")){
-					if(e.getCause().equals(DamageCause.FALL)){
-						e.setCancelled(true);
+		if(plugin.canAttack){
+			if((e.getEntity() instanceof Player)){
+				Player p = (Player)e.getEntity();
+				if(CTF.PlayerClasses.containsKey(p)){
+					if(CTF.PlayerClasses.get(p).equals("soldier")){
+						if(e.getCause().equals(DamageCause.FALL)){
+							e.setCancelled(true);
+						}
+					}
+				}
+				if(p.getHealth() - e.getDamage() < 1){
+					PlayerInventory inv = p.getInventory();
+					inv.clear();
+					inv.setArmorContents(null);
+					p.setHealth(0);
+				}
+	
+			}
+			if(e.getDamager().getType() == EntityType.ARROW){
+				//plugin.getServer().broadcastMessage("Entity = Arrow");
+				Arrow a = (Arrow)e.getDamager();
+				if(a.getShooter() instanceof Player){
+					//plugin.getServer().broadcastMessage("Shooter = Player");
+					if(e.getEntity() instanceof Player){
+						//plugin.getServer().broadcastMessage("Player = Player");
+						Player killer = (Player)a.getShooter();
+						Player player = (Player)e.getEntity();
+						if(killer.getLocation().distance(player.getLocation()) > 20){
+							player.sendMessage("Headshot!");
+							killer.sendMessage("Headshot!");
+							e.setDamage(20);
+						}
 					}
 				}
 			}
-			if(p.getHealth() - e.getDamage() < 1){
-				PlayerInventory inv = p.getInventory();
-				inv.clear();
-				inv.setArmorContents(null);
-				p.setHealth(0);
-			}
-
+		}else {
+			e.setCancelled(true);
 		}
 	}
 	
