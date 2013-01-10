19c5a61765510b4d772b02fda1e1ef4156478206
loseyjerry <jlosey90@gmail.com>
2013-01-10 13:31:21 -0500

    Not even sure what this is for...

---
+++ src/me/jimbo/plugin/InventoryOpenListener.java
@@ -0,0 +1,23 @@
+package me.jimbo.plugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+
+public class InventoryOpenListener implements Listener {
+
+	@SuppressWarnings("unused")
+	private CTF plugin;
+	
+	public InventoryOpenListener (CTF plugin) {
+		this.plugin = plugin;
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onInventoryOpen(InventoryOpenEvent e)
+	{
+		e.setCancelled(true);
+		// Do nothing
+	}
+}
