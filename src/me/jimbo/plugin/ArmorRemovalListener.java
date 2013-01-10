eedc59078dd66bd5936c86a355d8aa4b944f830f
loseyjerry <jlosey90@gmail.com>
2013-01-10 13:30:53 -0500

    Cannot remove armor.  Need this to accurately show flag on head when carrying it.

---
+++ src/me/jimbo/plugin/ArmorRemovalListener.java
@@ -0,0 +1,26 @@
+package me.jimbo.plugin;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType.SlotType;
+
+public class ArmorRemovalListener implements Listener {
+
+	
+	@SuppressWarnings("unused")
+	private CTF plugin;
+	
+	public ArmorRemovalListener (CTF plugin) {
+		this.plugin = plugin;
+	}
+	
+	@EventHandler (ignoreCancelled = true)
+	public void onArmorRemoval(InventoryClickEvent e){
+		if(e.getSlotType().equals(SlotType.ARMOR)&& !e.getCurrentItem().getType().equals(Material.AIR)){
+			e.setCancelled(true);
+		}
+		
+	}
+}
