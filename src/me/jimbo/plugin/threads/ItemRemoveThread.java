c5f68f72802ddce873ce5620e2cd7f78016781d7
loseyjerry <jlosey90@gmail.com>
2013-01-11 01:58:16 -0500

    Getting the hang of hiearchial packages

---
+++ src/me/jimbo/plugin/threads/ItemRemoveThread.java
@@ -0,0 +1,20 @@
+package me.jimbo.plugin.threads;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class ItemRemoveThread implements Runnable {
+	private Player p;
+	private ItemStack is;
+
+	public ItemRemoveThread(Player player, ItemStack items)
+	{
+	  this.p = player;
+	  this.is = items;
+	}
+
+	public void run()
+	{
+	  this.p.getInventory().removeItem(new ItemStack[] { this.is });
+	}
+}
