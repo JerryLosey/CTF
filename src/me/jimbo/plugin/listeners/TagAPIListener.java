b915016e8e6a3ee6efd02de2ccf968a96fd57342
loseyjerry <jlosey90@gmail.com>
2013-01-13 20:40:09 -0500

    TagAPI addon

---
+++ src/me/jimbo/plugin/listeners/TagAPIListener.java
@@ -0,0 +1,29 @@
+package me.jimbo.plugin.listeners;
+
+import me.jimbo.plugin.CTF;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.kitteh.tag.PlayerReceiveNameTagEvent;
+
+public class TagAPIListener implements Listener {
+
+	@SuppressWarnings("unused")
+	private CTF plugin;
+	
+	public TagAPIListener (CTF plugin) {
+		this.plugin = plugin;
+	}
+	
+    @EventHandler
+    public void onNameTag(PlayerReceiveNameTagEvent event) {
+    	if(CTF.AllPlayers.contains(event.getPlayer())){
+    		//Blue Team
+    		event.setTag(ChatColor.DARK_BLUE + event.getPlayer().getDisplayName());
+    	}else if(CTF.RedPlayers.contains(event.getPlayer())){
+    		//Red Team
+    		event.setTag(ChatColor.DARK_RED + event.getPlayer().getDisplayName());
+    	}
+    }
+}
