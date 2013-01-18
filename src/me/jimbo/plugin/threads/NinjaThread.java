7b28efbc4106f644abdbfc5a4f130b6ccf8e0b68
loseyjerry <jlosey90@gmail.com>
2013-01-18 14:39:23 -0500

    Begin to handle invisibility!

---
+++ src/me/jimbo/plugin/threads/NinjaThread.java
@@ -0,0 +1,36 @@
+package me.jimbo.plugin.threads;
+
+import me.jimbo.plugin.CTF;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class NinjaThread implements Runnable {
+	
+	private CTF plugin;
+
+	public NinjaThread(CTF plugin, Player player, Boolean visible){
+		this.plugin = plugin;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	    for (Player p : this.plugin.getServer().getOnlinePlayers())
+	    	if((p.getInventory().contains(331)) && (p.getItemInHand().getTypeId() == 331)){
+	    		p.getInventory().removeItem(new ItemStack[] { new ItemStack(331,1)});
+	    		togglev(p, false);
+	    	} else {
+	    		togglev(p, true);
+	    	}
+	}
+	
+	 private void togglev(Player p, boolean visible)
+	  {
+		 for(Player players : Bukkit.getOnlinePlayers()){
+			 players.hidePlayer(p);
+		 }
+	  }
+
+}
