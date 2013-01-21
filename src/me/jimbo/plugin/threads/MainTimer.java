76a4aea7cc7552d8d41cb16a4e361b53b20c1ca4
loseyjerry <jlosey90@gmail.com>
2013-01-20 22:21:40 -0500

    Create main timer for round limits

---
+++ src/me/jimbo/plugin/threads/MainTimer.java
@@ -0,0 +1,21 @@
+package me.jimbo.plugin.threads;
+
+import me.jimbo.plugin.CTF;
+
+public class MainTimer implements Runnable {
+
+	private CTF plugin;
+	public int x = 0;
+	
+	public MainTimer(CTF pl){
+		this.plugin = pl;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		// This is where we do whatever during the timer
+		//plugin.getServer().broadcastMessage("Test!");
+		x++;
+	}
+}
