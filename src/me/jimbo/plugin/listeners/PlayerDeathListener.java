8c2b5622df9bf6c637104e605030c55206f9ddd2
loseyjerry <jlosey90@gmail.com>
2013-01-24 13:53:37 -0500

    Adding Beserker feature.

--- src/me/jimbo/plugin/listeners/PlayerDeathListener.java
+++ src/me/jimbo/plugin/listeners/PlayerDeathListener.java
@@ -8,6 +8,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerDeathListener implements Listener {
 
@@ -23,9 +25,16 @@ public class PlayerDeathListener implements Listener {
 		e.getDrops().clear();
 		if(e.getEntity() instanceof Player) {
 			Player player = e.getEntity();
+			Player killer = e.getEntity().getKiller();
 			player.getPlayer().getInventory().clear();
 			player.getPlayer().getInventory().setArmorContents(null);
 			e.setDeathMessage(null);
+			// Beserker perk on kill
+			if((killer instanceof Player) && killer.hasPermission("ctf.class.beserker")){
+				killer.sendMessage(ChatColor.DARK_PURPLE + "Roid Rage");
+			    killer.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 420, 0));
+			    killer.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 420, 0));
+			}
 			if(CTF.RedPlayers.contains(player)){
 				if(plugin.blueFlagCarrier == player){
 					plugin.getServer().broadcastMessage(ChatColor.DARK_RED + player.getDisplayName() + ChatColor.WHITE + " dropped the " + ChatColor.BLUE + "blue " + ChatColor.WHITE + "flag!");
