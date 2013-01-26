53f0fb25d8e8a136b7eb0a084ca5875404621579
loseyjerry <jlosey90@gmail.com>
2013-01-26 14:23:02 -0500

    Martyrs explode when they die.  Need to change explosion when Bukkit updates.

--- src/me/jimbo/plugin/listeners/PlayerDeathListener.java
+++ src/me/jimbo/plugin/listeners/PlayerDeathListener.java
@@ -3,6 +3,7 @@ package me.jimbo.plugin.listeners;
 import me.jimbo.plugin.CTF;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -10,6 +11,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 //import org.bukkit.potion.PotionEffect;
 //import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerDeathListener implements Listener {
 
@@ -25,16 +28,21 @@ public class PlayerDeathListener implements Listener {
 		e.getDrops().clear();
 		if(e.getEntity() instanceof Player) {
 			Player player = e.getEntity();
-//			Player killer = e.getEntity().getKiller();
+			Player killer = e.getEntity().getKiller();
 			player.getPlayer().getInventory().clear();
 			player.getPlayer().getInventory().setArmorContents(null);
 			e.setDeathMessage(null);
+			
 			// Beserker perk on kill
-//			if((killer instanceof Player) && killer.hasPermission("ctf.class.beserker")){
-//				killer.sendMessage(ChatColor.DARK_PURPLE + "Roid Rage");
-//			    killer.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 420, 0));
-//			    killer.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 420, 0));
-//			}
+			if((killer instanceof Player) && CTF.PlayerClasses.get(player).equalsIgnoreCase("berserker")){
+				killer.sendMessage(ChatColor.DARK_PURPLE + "Roid Rage");
+			    killer.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 420, 0));
+			    killer.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 420, 0));
+			}
+			if(CTF.PlayerClasses.get(player).equalsIgnoreCase("martyr")){
+				Location loc = player.getLocation();
+				plugin.getServer().getWorld("world").createExplosion(loc, 2.0F, false);// Add another ",false" to the end when updating to 1.4.7+
+			}
 			if(CTF.RedPlayers.contains(player)){
 				if(plugin.blueFlagCarrier == player){
 					plugin.getServer().broadcastMessage(ChatColor.DARK_RED + player.getDisplayName() + ChatColor.WHITE + " dropped the " + ChatColor.BLUE + "blue " + ChatColor.WHITE + "flag!");
