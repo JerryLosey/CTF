d35119917c772564d56bb9cf19f6e206203d49d7
loseyjerry <jlosey90@gmail.com>
2013-01-25 21:34:07 -0500

    Berserkers get a cool boost!

--- src/me/jimbo/plugin/threads/PotionEffectTimer.java
+++ src/me/jimbo/plugin/threads/PotionEffectTimer.java
@@ -1,6 +1,8 @@
 package me.jimbo.plugin.threads;
 
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import me.jimbo.plugin.CTF;
 
@@ -12,12 +14,20 @@ public class PotionEffectTimer implements Runnable {
 		this.plugin = plugin;
 	}
 
-	@SuppressWarnings("unused")
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		for (Player p : this.plugin.getServer().getOnlinePlayers()){
 			// Do something
+			if(p.hasPermission("ctf.class.berserker")){
+				p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 6000, 1) );
+				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 6000, 1) );
+				p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 6000, 1) );
+			}else{
+				for(PotionEffect effect : p.getActivePotionEffects()){
+					p.removePotionEffect(effect.getType());
+				}
+			}
 		}
 		
 	}
